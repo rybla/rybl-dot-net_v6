03eaca5dc7e987b5400d21f44871facee5dc8a5c
Henry Blanchette <henry@monkeyface.com>
2025-09-06 01:17:13 -0400

    dev flag

--- src/Blog/Config.hs
+++ src/Blog/Config.hs
@@ -1,1 +1,15 @@
-module Blog.Config () where
+{-# LANGUAGE CPP #-}
+{-# OPTIONS_GHC -Wno-missing-export-lists #-}
+
+module Blog.Config where
+
+data Mode = Development | Production
+  deriving (Show, Eq)
+
+#if defined(DEVELOPMENT)
+mode :: Mode
+mode = Development
+#else
+mode :: Mode
+mode = Production
+#endif
