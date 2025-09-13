d159c7b9c0aab6fd7c9e1b4b81ba45c944275995
Henry Blanchette <henry@monkeyface.com>
2025-09-13 17:25:44 -0400

    Create basic HTML and CSS templates
    This commit adds the basic HTML and CSS templates needed for the blog.
    The templates are for the index page, header, footer, and post pages.
    Also creates a common css file.

---
+++ src/Blog/Print/Index.hs
@@ -0,0 +1,99 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TupleSections #-}
+
+module Blog.Print.Index (printIndex) where
+
+import Blog.Common
+import qualified Blog.Pandoc as Pandoc
+import qualified Blog.Paths as Paths
+import Blog.Print.Common
+import Blog.Utility
+import Control.Lens hiding (index)
+import Control.Monad.Except (MonadError)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State (MonadState)
+import Control.Monad.Writer (MonadIO)
+import qualified Data.Aeson as Aeson
+import qualified Data.Aeson.Types as Aeson
+import Data.Default (def)
+import qualified Data.Text.IO as TextIO
+import System.FilePath ((</>))
+import Text.Pandoc (Pandoc)
+import qualified Text.Pandoc as Pandoc
+import Text.PrettyPrint.HughesPJClass (Doc, text, (<+>))
+
+printIndex ::
+  (MonadIO m, MonadError Doc m, MonadState env m) =>
+  [Post] ->
+  m ()
+printIndex posts = evalIsoStateT (pairIso def) do
+  index <- makeIndex posts
+
+  templateText <- TextIO.readFile (Paths.offline.template.here </> ("index" & toHtmlFileName)) & liftIO
+
+  contentHtml <- Pandoc.writeHtml5String Pandoc.def index & Pandoc.lensPandocM _1
+
+  indexTemplate <-
+    Pandoc.compileTemplate mempty templateText
+      & unBlogTemplateMonad
+      >>= fromEither (("compileTemplate:" <+>) . text)
+
+  indexHtml <- do
+    vars <-
+      Aeson.parseEither
+        Aeson.parseJSON
+        ( Aeson.object
+            [("content", contentHtml & Aeson.toJSON)]
+        )
+        & fromEither (("Error when parsing template variables JSON:" <+>) . text)
+    Pandoc.writeHtml5String
+      ( def
+          { Pandoc.writerTemplate = Just indexTemplate,
+            Pandoc.writerVariables = vars,
+            Pandoc.writerHTMLMathMethod = Pandoc.MathJax ""
+          }
+      )
+      index
+      & Pandoc.lensPandocM _1
+
+  TextIO.writeFile (Paths.offline.here </> ("index" & toHtmlFileName)) indexHtml & liftIO
+
+makeIndex ::
+  (MonadIO m, MonadError Doc m) =>
+  [Post] ->
+  m Pandoc
+makeIndex posts = do
+  postCards <- posts & traverse makePostCard
+  return $
+    Pandoc.Pandoc
+      mempty
+      ( concat
+          [ [Pandoc.Header 1 mempty [Pandoc.Str "Index"]],
+            postCards
+          ]
+      )
+
+makePostCard :: (MonadError Doc m) => Post -> m Pandoc.Block
+makePostCard post = do
+  postHref <- post.postId & toPostHref
+  return
+    $ Pandoc.Div
+      (mempty & Pandoc.attrClasses %~ (["PostCard"] ++))
+      . concat
+    $ [ [ Pandoc.Div
+            (mempty & Pandoc.attrClasses %~ (["title"] ++))
+            [ Pandoc.Plain
+                [ Pandoc.Link
+                    mempty
+                    [Pandoc.Str post.postTitle]
+                    (showText postHref, mempty)
+                ]
+            ]
+        ],
+        [ Pandoc.Div
+            (mempty & Pandoc.attrClasses %~ (["abstract"] ++))
+            [ Pandoc.Plain [Pandoc.Str abstract]
+            ]
+        | abstract <- post.postAbstract & refold
+        ]
+      ]
